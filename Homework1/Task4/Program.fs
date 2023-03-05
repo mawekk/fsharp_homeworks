49560ace653c2d6174cb6b182a453140bed161db
mawekk <mariamya243@gmail.com>
2023-03-05 04:39:43 +0300

    add solutions for homework 1

---
+++ Homework1/Task4/Program.fs
@@ -0,0 +1,6 @@
+﻿let listOfPowersOfTwo n m =
+    let rec listOfPowersOfTwo list m acc =
+        if m = 0 then list else listOfPowersOfTwo (acc :: list) (m - 1) (acc * 2)
+    List.rev <| listOfPowersOfTwo [] (m + 1) (pown 2 n)
+    
+printf "%A" <| listOfPowersOfTwo 10 5
---
+++ Homework1/Task5/Program.fs
@@ -0,0 +1,9 @@
+﻿let findFirstEntrance element list =
+    let rec findFirstEntrance element list index =
+        match list with
+        | [] -> None
+        | head::_ when head = element -> Some index
+        | _::tail -> findFirstEntrance element tail (index + 1)
+    findFirstEntrance element list 0
+
+printf $"{(findFirstEntrance 5 [1; 2; 5; 3; 5]).Value}"
